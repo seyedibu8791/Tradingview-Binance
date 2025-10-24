1b638947b99396ce5be8a808aab8f31342b96784
seyedibu8791 <105744059+seyedibu8791@users.noreply.github.com>
2025-10-24 21:24:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -29,3 +29,7 @@ TRADE_AMOUNT      = float(os.getenv("TRADE_AMOUNT", "50"))
 LEVERAGE          = int(os.getenv("LEVERAGE", "20"))
 MARGIN_TYPE       = os.getenv("MARGIN_TYPE", "ISOLATED")
 LOG_FILLED_PRICE  = True   # log actual market order fill price
+
+EXIT_ORDER_TYPE   = os.getenv("EXIT_ORDER_TYPE", "LIMIT")  # LIMIT or MARKET
+EXIT_ORDER_DELAY  = int(os.getenv("EXIT_ORDER_DELAY", "5")) # in seconds, for MARKET exit
+
